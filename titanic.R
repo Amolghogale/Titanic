8146f1f1a5c7721b06a243d28131ef1ba4c7e3a4
Amol Ghogale <amol.ghogale70@gmail.com>
2017-06-23 16:52:46 +0530

    Add files via upload

---
+++ titanic.R
@@ -0,0 +1,56 @@
+setwd("C:/Users/Amith/Desktop/kaggle titanic")
+titanic.train <- read.csv(file="train.csv",stringsAsFactors = FALSE,header=TRUE)
+titanic.test <- read.csv(file="test.csv",stringsAsFactors = FALSE,header=TRUE)
+
+titanic.train$IsTrainSet <- TRUE
+titanic.test$IsTrainSet <- FALSE
+
+
+titanic.test$Survived <- NA
+
+
+titanic.full <- rbind(titanic.train, titanic.test)
+
+titanic.full[titanic.full$Embarked=='',"Embarked"] <- 'S'
+
+table(is.na(titanic.full$Age))
+
+age.median <-median(titanic.full$Age, na.rm = TRUE)
+
+titanic.full[is.na(titanic.full$Age),"Age"] <- age.median
+
+table(is.na(titanic.full$Fare))
+# clean missing values of Fare
+fare.median <-median(titanic.full$Fare, na.rm = TRUE)
+titanic.full[is.na(titanic.full$Fare),"Fare"] <- fare.median
+
+# categorical casting
+titanic.full$Pclass <- as.factor(titanic.full$Pclass)
+titanic.full$Sex <- as.factor(titanic.full$Sex)
+titanic.full$Embarked <- as.factor(titanic.full$Embarked)
+
+
+# split data set out into train and test
+titanic.train<-titanic.full[titanic.full$IsTrainSet==TRUE,]
+titanic.test<-titanic.full[titanic.full$IsTrainSet==FALSE,]
+
+titanic.train$Survived <- as.factor(titanic.train$Survived)
+
+Survived.equation <-"Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+Survived.formula <- as.formula(Survived.equation)
+install.packages("randomForest")
+library(randomForest)
+
+titanic.model<- randomForest(formula = Survived.formula,data = titanic.train,ntree = 500,mtry = 3,nodesize = 0.01*nrow(titanic.test))
+
+feature.equation <-"Pclass + Sex + Age + SibSp + Parch + Fare + Embarked"
+
+
+Survived <- predict(titanic.model, newdata = titanic.test)
+
+PassengerId <- titanic.test$PassengerId
+output.df <- as.data.frame(PassengerId)
+output.df$Survived <- Survived
+
+
+write.csv(output.df, "kaggle_submission.csv",row.names=FALSE)
